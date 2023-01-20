422c24cef0cadd92a416e19cf7452744bb8522c6
Andras Hummer <andras.hummer@dpc.hu>
2023-01-20 11:23:39 +0100

    Fee also moved to conversion account

--- src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/TransferToConversionAccountInAmsWorker.java
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/TransferToConversionAccountInAmsWorker.java
@@ -1,7 +1,9 @@
 package org.mifos.connector.ams.zeebe.workers.bookamount;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -9,8 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
+import iso.std.iso20022plus.tech.json.pain_001_001.CreditTransferTransaction40;
+import iso.std.iso20022plus.tech.json.pain_001_001.Pain00100110CustomerCreditTransferInitiationV10MessageSchema;
 
 @Component
 public class TransferToConversionAccountInAmsWorker extends AbstractMoneyInOutWorker {
@@ -21,19 +27,45 @@ public class TransferToConversionAccountInAmsWorker extends AbstractMoneyInOutWo
 	@Value("${fineract.paymentType.paymentTypeExchangeToFiatCurrencyId}")
 	private Integer paymentTypeExchangeToFiatCurrencyId;
 	
+	@Value("${fineract.paymentType.paymentTypeFeeId}")
+	private Integer paymentTypeFeeId;
+	
 	private static final DateTimeFormatter PATTERN = DateTimeFormatter.ofPattern(FORMAT);
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public void handle(JobClient jobClient, ActivatedJob activatedJob) throws Exception {
 		logger.error("Debtor exchange worker starting");
 		try {
 			Map<String, Object> variables = activatedJob.getVariablesAsMap();
 			
+			String originalPain001 = (String) variables.get("originalPain001");
+			
+			ObjectMapper om = new ObjectMapper();
+			Pain00100110CustomerCreditTransferInitiationV10MessageSchema pain001 = om.readValue(originalPain001, Pain00100110CustomerCreditTransferInitiationV10MessageSchema.class);
+			
+			BigDecimal amount = BigDecimal.ZERO;
+			BigDecimal fee = BigDecimal.ZERO;
+			
+			List<CreditTransferTransaction40> creditTransferTransactionInformation = pain001.getDocument().getPaymentInformation().get(0).getCreditTransferTransactionInformation();
+			
+			for (CreditTransferTransaction40 ctti : creditTransferTransactionInformation) {
+				BigDecimal sum = new BigDecimal(ctti.getAmount().getInstructedAmount().getAmount().toString());
+				Map<String, Object> additionalProperties = ctti.getSupplementaryData().get(0).getEnvelope().getAdditionalProperties();
+				if (additionalProperties.containsKey("OrderManagerSupplementaryData")) {
+					Map<String, Object> orderManagerSupplementaryData = (Map<String, Object>) additionalProperties.get("OrderManagerSupplementaryData");
+					if (orderManagerSupplementaryData.containsKey("ParentInternalCorrelationId")) {
+						fee = sum;
+					} else {
+						amount = sum;
+					}
+				}
+			}
+			
 			logger.error("Debtor exchange worker incoming variables:");
 			variables.entrySet().forEach(e -> logger.error("{}: {}", e.getKey(), e.getValue()));
 		
 			String transactionDate = LocalDate.now().format(PATTERN);
-			Object amount = variables.get("amount");
 			
 			logger.info("Attempting to deposit the amount of {}", amount);
 		
@@ -41,8 +73,26 @@ public class TransferToConversionAccountInAmsWorker extends AbstractMoneyInOutWo
 			Integer conversionAccountAmsId = (Integer) variables.get("conversionAccountAmsId");
 			
 			logger.info("Attempting to withdraw from account {}", disposalAccountAmsId);
-		
+			
 			ResponseEntity<Object> responseObject = withdraw(transactionDate, amount, disposalAccountAmsId, paymentTypeExchangeECurrencyId);
+				
+			if (!HttpStatus.OK.equals(responseObject.getStatusCode())) {
+				jobClient.newFailCommand(activatedJob.getKey()).retries(0).send();
+				return;
+			}
+				
+			responseObject = withdraw(transactionDate, fee, disposalAccountAmsId, paymentTypeFeeId);
+				
+			if (!HttpStatus.OK.equals(responseObject.getStatusCode())) {
+				jobClient.newFailCommand(activatedJob.getKey()).retries(0).send();
+				ResponseEntity<Object> sagaResponseObject = deposit(transactionDate, amount, disposalAccountAmsId, paymentTypeExchangeECurrencyId);
+					
+				if (!HttpStatus.OK.equals(sagaResponseObject.getStatusCode())) {
+					jobClient.newFailCommand(activatedJob.getKey()).retries(0).send();
+					return;
+				}
+				return;
+			}
 		
 			if (!HttpStatus.OK.equals(responseObject.getStatusCode())) {
 				jobClient.newFailCommand(activatedJob.getKey()).retries(0).send();
