24f37a2e19c6c5075111bd64a3d0bda04aadd3fe
dhruvsonagara <dhruvsonagara@gmail.com>
2023-02-03 18:30:24 +0530

    changed the logger to debug for sensitive info

--- src/main/java/org/mifos/connector/ams/interop/PrepareTransferRequest.java
+++ src/main/java/org/mifos/connector/ams/interop/PrepareTransferRequest.java
@@ -88,7 +88,7 @@ public class PrepareTransferRequest implements Processor {
                     TransactionRole.valueOf(exchange.getProperty(TRANSACTION_ROLE, String.class)));
         }
 
-        logger.info("prepared transferRequestDTO: {}", objectMapper.writeValueAsString(transferRequestDTO));
+        logger.debug("prepared transferRequestDTO: {}", objectMapper.writeValueAsString(transferRequestDTO));
         exchange.getIn().setBody(transferRequestDTO);
     }
 }
