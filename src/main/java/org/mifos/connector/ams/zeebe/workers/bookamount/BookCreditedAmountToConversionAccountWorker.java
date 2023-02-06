c153723aa79cedca46f0af197cacce1db14e6bd6
Andras Hummer <andras.hummer@dpc.hu>
2023-02-06 16:43:37 +0100

    pacs.008 to camt.052

--- src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/BookCreditedAmountToConversionAccountWorker.java
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/BookCreditedAmountToConversionAccountWorker.java
@@ -9,6 +9,8 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.jboss.logging.MDC;
+import org.mifos.connector.ams.mapstruct.Pacs008Camt052Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -17,12 +19,13 @@ import eu.nets.realtime247.ri_2015_10.ObjectFactory;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import iso.std.iso._20022.tech.xsd.pacs_002_001.Document;
+import iso.std.iso20022plus.tech.json.camt_052_001.BankToCustomerAccountReportV08;
 
 @Component
 public class BookCreditedAmountToConversionAccountWorker extends AbstractMoneyInOutWorker {
 	
-	// @Autowired
-	// private Pain001Camt052Mapper camt052Mapper;
+	@Autowired
+	private Pacs008Camt052Mapper camt052Mapper;
 	
 	private static final DateTimeFormatter PATTERN = DateTimeFormatter.ofPattern(FORMAT);
 
@@ -35,10 +38,11 @@ public class BookCreditedAmountToConversionAccountWorker extends AbstractMoneyIn
 			logger.info("Incoming money worker started with variables");
 			variables.keySet().forEach(logger::info);
 			
-			// String originalPain001 = (String) variables.get("originalPain001");
-			// ObjectMapper om = new ObjectMapper();
-			// Pain00100110CustomerCreditTransferInitiationV10MessageSchema pain001 = om.readValue(originalPain001, Pain00100110CustomerCreditTransferInitiationV10MessageSchema.class);
-		
+			String originalPacs008 = (String) variables.get("originalPacs008");
+			JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class);
+			JAXBElement<iso.std.iso._20022.tech.xsd.pacs_008_001.Document> object = (JAXBElement<iso.std.iso._20022.tech.xsd.pacs_008_001.Document>) jc.createUnmarshaller().unmarshal(new StringReader(originalPacs008));
+			iso.std.iso._20022.tech.xsd.pacs_008_001.Document pacs008 = object.getValue();
+			
 			String internalCorrelationId = (String) variables.get("internalCorrelationId");
 			MDC.put("internalCorrelationId", internalCorrelationId);
 		
@@ -47,7 +51,6 @@ public class BookCreditedAmountToConversionAccountWorker extends AbstractMoneyIn
 			
 			logger.info("Worker to book incoming money in AMS has started with incoming pacs.002 {} for tenant {}", originalPacs002, tenantId);
 			
-			JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class);
 			JAXBElement<Document> jaxbObject = (JAXBElement<Document>) jc.createUnmarshaller().unmarshal(new StringReader(originalPacs002));
 			Document pacs_002 = jaxbObject.getValue();
 			
@@ -60,11 +63,11 @@ public class BookCreditedAmountToConversionAccountWorker extends AbstractMoneyIn
 			
 			ResponseEntity<Object> responseObject = deposit(transactionDate, amount, conversionAccountAmsId, 1, tenantId);
 			
-			// BankToCustomerAccountReportV08 convertedCamt052 = camt052Mapper.toCamt052(pain001.getDocument());
-			// String camt052 = om.writeValueAsString(convertedCamt052);
+			BankToCustomerAccountReportV08 convertedCamt052 = camt052Mapper.toCamt052(pacs008);
+			String camt052 = convertedCamt052.toString();
 			
-			// logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  camt.052  <<<<<<<<<<<<<<<<<<<<<<<<");
-			// logger.info("The following camt.052 will be inserted into the data table: {}", camt052);
+			logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  camt.052  <<<<<<<<<<<<<<<<<<<<<<<<");
+			logger.info("The following camt.052 will be inserted into the data table: {}", camt052);
 		
 			if (HttpStatus.OK.equals(responseObject.getStatusCode())) {
 				logger.info("Worker to book incoming money in AMS has finished successfully");
--- src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/TransferToDisposalAccountWorker.java
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/TransferToDisposalAccountWorker.java
@@ -1,27 +1,29 @@
 package org.mifos.connector.ams.zeebe.workers.bookamount;
 
+import java.io.StringReader;
 import java.util.Map;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+
 import org.jboss.logging.MDC;
-import org.mifos.connector.ams.mapstruct.Pain001Camt052Mapper;
+import org.mifos.connector.ams.mapstruct.Pacs008Camt052Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import eu.nets.realtime247.ri_2015_10.ObjectFactory;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import iso.std.iso20022plus.tech.json.camt_052_001.BankToCustomerAccountReportV08;
-import iso.std.iso20022plus.tech.json.pain_001_001.Pain00100110CustomerCreditTransferInitiationV10MessageSchema;
 
 @Component
 public class TransferToDisposalAccountWorker extends AbstractMoneyInOutWorker {
 	
 	@Autowired
-	private Pain001Camt052Mapper camt052Mapper;
+	private Pacs008Camt052Mapper camt052Mapper;
 	
 	@Value("${fineract.paymentType.paymentTypeExchangeFiatCurrencyId}")
 	private Integer paymentTypeExchangeFiatCurrencyId;
@@ -34,9 +36,10 @@ public class TransferToDisposalAccountWorker extends AbstractMoneyInOutWorker {
 		try {
 			Map<String, Object> variables = activatedJob.getVariablesAsMap();
 			
-			String originalPain001 = (String) variables.get("originalPain001");
-			ObjectMapper om = new ObjectMapper();
-			Pain00100110CustomerCreditTransferInitiationV10MessageSchema pain001 = om.readValue(originalPain001, Pain00100110CustomerCreditTransferInitiationV10MessageSchema.class);
+			String originalPacs008 = (String) variables.get("originalPacs008");
+			JAXBContext jc = JAXBContext.newInstance(ObjectFactory.class);
+			JAXBElement<iso.std.iso._20022.tech.xsd.pacs_008_001.Document> object = (JAXBElement<iso.std.iso._20022.tech.xsd.pacs_008_001.Document>) jc.createUnmarshaller().unmarshal(new StringReader(originalPacs008));
+			iso.std.iso._20022.tech.xsd.pacs_008_001.Document pacs008 = object.getValue();
 		
 			String internalCorrelationId = (String) variables.get("internalCorrelationId");
 			MDC.put("internalCorrelationId", internalCorrelationId);
@@ -60,8 +63,8 @@ public class TransferToDisposalAccountWorker extends AbstractMoneyInOutWorker {
 		
 			responseObject = deposit(transactionDate, amount, disposalAccountAmsId, paymentTypeIssuingECurrencyId, tenantId);
 			
-			BankToCustomerAccountReportV08 convertedCamt052 = camt052Mapper.toCamt052(pain001.getDocument());
-			String camt052 = om.writeValueAsString(convertedCamt052);
+			BankToCustomerAccountReportV08 convertedCamt052 = camt052Mapper.toCamt052(pacs008);
+			String camt052 = convertedCamt052.toString();
 			
 			logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  camt.052  <<<<<<<<<<<<<<<<<<<<<<<<");
 			logger.info("The following camt.052 will be inserted into the data table: {}", camt052);
