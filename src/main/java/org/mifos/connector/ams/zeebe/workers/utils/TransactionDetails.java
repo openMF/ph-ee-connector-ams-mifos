bc26eb8dfc13c1e93283e279828d6640aef27684
Andras Hummer <andras.hummer@dpc.hu>
2023-03-22 09:46:20 +0100

    Renamed field to structuredTransactionDetails

--- src/main/java/org/mifos/connector/ams/zeebe/workers/utils/TransactionDetails.java
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/utils/TransactionDetails.java
@@ -2,5 +2,5 @@ package org.mifos.connector.ams.zeebe.workers.utils;
 
 import java.time.LocalDateTime;
 
-public record TransactionDetails(Object savings_account_id, Object amsTransactionId, String internalCorrelationId, String camt052, LocalDateTime created_at, LocalDateTime updated_at) {
+public record TransactionDetails(Object savings_account_id, Object amsTransactionId, String internalCorrelationId, String structuredTransactionDetails, LocalDateTime created_at, LocalDateTime updated_at) {
 }
