81c20314bdca74568f603c0daf18742a3860e3b0
Andras Hummer <andras.hummer@dpc.hu>
2023-04-06 11:12:51 +0200

    Rename camt.052 fields

--- src/main/java/org/mifos/connector/ams/zeebe/workers/utils/TransactionDetails.java
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/utils/TransactionDetails.java
@@ -2,5 +2,12 @@ package org.mifos.connector.ams.zeebe.workers.utils;
 
 import java.time.LocalDateTime;
 
-public record TransactionDetails(Object savings_account_id, Object amsTransactionId, String internalCorrelationId, String structuredTransactionDetails, LocalDateTime created_at, LocalDateTime updated_at) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record TransactionDetails(Object savings_account_id, 
+		@JsonProperty("ams_transaction_id") Object amsTransactionId, 
+		@JsonProperty("internal_correlation_id") String internalCorrelationId, 
+		@JsonProperty("structured_transaction_details") String structuredTransactionDetails, 
+		LocalDateTime created_at, 
+		LocalDateTime updated_at) {
 }
