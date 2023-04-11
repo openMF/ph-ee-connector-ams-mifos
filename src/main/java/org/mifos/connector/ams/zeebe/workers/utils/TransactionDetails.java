40124d90688fc980d8ece9fe85931eadd94a9023
Andras Hummer <andras.hummer@dpc.hu>
2023-04-11 10:38:40 +0200

    Fix transaction details

--- src/main/java/org/mifos/connector/ams/zeebe/workers/utils/TransactionDetails.java
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/utils/TransactionDetails.java
@@ -1,13 +1,8 @@
 package org.mifos.connector.ams.zeebe.workers.utils;
 
-import java.time.LocalDateTime;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record TransactionDetails(Object savings_account_id, 
-		@JsonProperty("ams_transaction_id") Object amsTransactionId, 
+public record TransactionDetails(@JsonProperty("ams_transaction_id") Object amsTransactionId, 
 		@JsonProperty("internal_correlation_id") String internalCorrelationId, 
-		@JsonProperty("structured_transaction_details") String structuredTransactionDetails, 
-		LocalDateTime created_at, 
-		LocalDateTime updated_at) {
+		@JsonProperty("structured_transaction_details") String structuredTransactionDetails) {
 }
