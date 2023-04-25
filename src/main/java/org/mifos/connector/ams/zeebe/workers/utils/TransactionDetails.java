9620e63882868461ac2d5143e1d86b90e57df7c6
Andras Hummer <andras.hummer@dpc.hu>
2023-04-25 13:56:16 +0200

    Tx group Id, category purpose code and fee category purpose code added

--- src/main/java/org/mifos/connector/ams/zeebe/workers/utils/TransactionDetails.java
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/utils/TransactionDetails.java
@@ -4,5 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record TransactionDetails(@JsonProperty("ams_transaction_id") Object amsTransactionId, 
 		@JsonProperty("internal_correlation_id") String internalCorrelationId, 
-		@JsonProperty("structured_transaction_details") String structuredTransactionDetails) {
+		@JsonProperty("structured_transaction_details") String structuredTransactionDetails,
+		@JsonProperty("transaction_group_id") String transactionGroupId,
+		@JsonProperty("category_purpose_code") String categoryPurposeCode) {
 }
