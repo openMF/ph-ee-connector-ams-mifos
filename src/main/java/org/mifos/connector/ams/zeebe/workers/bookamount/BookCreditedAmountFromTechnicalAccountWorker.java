f7fea460098ca0d1ce22a053cc8ff877087525a0
Andras Hummer <andras.hummer@dpc.hu>
2023-07-18 11:57:21 +0200

    NEOB-1141: Added new worker to return amount from technical account to conversion account

---
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/BookCreditedAmountFromTechnicalAccountWorker.java
@@ -0,0 +1,168 @@
+package org.mifos.connector.ams.zeebe.workers.bookamount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mifos.connector.ams.fineract.Config;
+import org.mifos.connector.ams.fineract.ConfigFactory;
+import org.mifos.connector.ams.mapstruct.Pacs008Camt053Mapper;
+import org.mifos.connector.ams.zeebe.workers.utils.BatchItemBuilder;
+import org.mifos.connector.ams.zeebe.workers.utils.JAXBUtils;
+import org.mifos.connector.ams.zeebe.workers.utils.TransactionBody;
+import org.mifos.connector.ams.zeebe.workers.utils.TransactionDetails;
+import org.mifos.connector.ams.zeebe.workers.utils.TransactionItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import io.camunda.zeebe.spring.client.annotation.Variable;
+import io.camunda.zeebe.spring.client.exception.ZeebeBpmnError;
+import iso.std.iso._20022.tech.json.camt_053_001.ReportEntry10;
+
+@Component
+public class BookCreditedAmountFromTechnicalAccountWorker extends AbstractMoneyInOutWorker {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Value("${fineract.incoming-money-api}")
+	private String incomingMoneyApi;
+	
+	@Value("${fineract.locale}")
+	private String locale;
+	
+	@Autowired
+	private BatchItemBuilder batchItemBuilder;
+	
+	@Autowired
+    private ConfigFactory paymentTypeConfigFactory;
+	
+	@Autowired
+	private ConfigFactory technicalAccountConfigFactory;
+	
+	@Autowired
+	private JAXBUtils jaxbUtils;
+	
+	@Autowired
+    private Pacs008Camt053Mapper camt053Mapper;
+	
+	private static final String FORMAT = "yyyyMMdd";
+
+	@JobWorker
+	public void bookCreditedAmountFromTechnicalAccount(JobClient jobClient, 
+    		ActivatedJob activatedJob,
+    		@Variable String originalPacs008,
+			@Variable String amount,
+			@Variable String tenantIdentifier,
+			@Variable String paymentScheme,
+			@Variable String transactionDate,
+			@Variable String currency,
+			@Variable String internalCorrelationId,
+			@Variable String transactionGroupId,
+			@Variable String transactionCategoryPurposeCode,
+			@Variable String caseIdentifier,
+			@Variable Integer conversionAccountAmsId
+			) {
+		
+		try {
+            logger.info("Incoming money worker started with variables");
+            
+            ObjectMapper objectMapper = new ObjectMapper();
+            
+            List<TransactionItem> items = new ArrayList<>();
+
+            iso.std.iso._20022.tech.xsd.pacs_008_001.Document pacs008 = jaxbUtils.unmarshalPacs008(originalPacs008);
+
+            MDC.put("internalCorrelationId", internalCorrelationId);
+
+            batchItemBuilder.tenantId(tenantIdentifier);
+            
+            Config technicalAccountConfig = technicalAccountConfigFactory.getConfig(tenantIdentifier);
+            
+            String taLookup = String.format("%s.%s", paymentScheme, caseIdentifier);
+            logger.debug("Looking up account id for {}", taLookup);
+			Integer recallTechnicalAccountId = technicalAccountConfig.findByOperation(taLookup);
+    		
+    		String technicalAccountWithdrawalRelativeUrl = String.format("%s%d/transactions?command=%s", incomingMoneyApi.substring(1), recallTechnicalAccountId, "withdraw");
+    		
+    		Config paymentTypeConfig = paymentTypeConfigFactory.getConfig(tenantIdentifier);
+    		Integer paymentTypeId = paymentTypeConfig.findByOperation(String.format("%s.%s.%s", paymentScheme, "bookFromTechnicalAccount", caseIdentifier));
+    		
+    		TransactionBody body = new TransactionBody(
+    				transactionDate,
+    				amount,
+    				paymentTypeId,
+    				"",
+    				FORMAT,
+    				locale);
+    		
+    		String bodyItem = objectMapper.writeValueAsString(body);
+    		
+    		batchItemBuilder.add(items, technicalAccountWithdrawalRelativeUrl, bodyItem, false);
+    	
+    		ReportEntry10 convertedCamt053Entry = camt053Mapper.toCamt053Entry(pacs008);
+    		String camt053Entry = objectMapper.writeValueAsString(convertedCamt053Entry);
+    		
+    		String camt053RelativeUrl = String.format("datatables/transaction_details/%d", recallTechnicalAccountId);
+    		
+    		TransactionDetails td = new TransactionDetails(
+    				"$.resourceId",
+    				internalCorrelationId,
+    				camt053Entry,
+    				transactionGroupId,
+    				transactionCategoryPurposeCode);
+    		
+    		String camt053Body = objectMapper.writeValueAsString(td);
+
+    		batchItemBuilder.add(items, camt053RelativeUrl, camt053Body, true);
+    		
+    		
+    		
+    		String conversionAccountDepositRelativeUrl = String.format("%s%d/transactions?command=%s", incomingMoneyApi.substring(1), conversionAccountAmsId, "deposit");
+    		
+    		paymentTypeId = paymentTypeConfig.findByOperation(String.format("%s.%s.%s", paymentScheme, "bookCreditedAmountToConversionAccount", "ConversionAccount.DepositTransactionAmount"));
+    		
+    		body = new TransactionBody(
+    				transactionDate,
+    				amount,
+    				paymentTypeId,
+    				"",
+    				FORMAT,
+    				locale);
+    		
+    		bodyItem = objectMapper.writeValueAsString(body);
+    		
+    		batchItemBuilder.add(items, conversionAccountDepositRelativeUrl, bodyItem, false);
+    	
+    		convertedCamt053Entry = camt053Mapper.toCamt053Entry(pacs008);
+    		camt053Entry = objectMapper.writeValueAsString(convertedCamt053Entry);
+    		
+    		camt053RelativeUrl = String.format("datatables/transaction_details/%d", conversionAccountAmsId);
+    		
+    		td = new TransactionDetails(
+    				"$.resourceId",
+    				internalCorrelationId,
+    				camt053Entry,
+    				transactionGroupId,
+    				transactionCategoryPurposeCode);
+    		
+    		camt053Body = objectMapper.writeValueAsString(td);
+
+    		batchItemBuilder.add(items, camt053RelativeUrl, camt053Body, true);
+
+            doBatch(items, tenantIdentifier, internalCorrelationId);
+        } catch (Exception e) {
+            logger.error("Worker to book incoming money in AMS has failed, dispatching user task to handle fiat deposit", e);
+            throw new ZeebeBpmnError("Error_BookToConversionToBeHandledManually", e.getMessage());
+        } finally {
+            MDC.remove("internalCorrelationId");
+        }
+	}
+}
