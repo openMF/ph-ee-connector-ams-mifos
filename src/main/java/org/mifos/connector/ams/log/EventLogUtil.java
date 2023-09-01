d5e6420e423e4a4b45e97f4217d312e93907fc28
GeluHUN <peter.gelei@dpc.hu>
2023-09-01 11:23:14 +0200

    NEOB-1239

--- src/main/java/org/mifos/connector/ams/log/EventLogUtil.java
+++ src/main/java/org/mifos/connector/ams/log/EventLogUtil.java
@@ -3,6 +3,7 @@ package org.mifos.connector.ams.log;
 import com.baasflow.commons.events.Event;
 import com.baasflow.commons.events.EventLogLevel;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
+import lombok.experimental.UtilityClass;
 import org.mifos.connector.ams.zeebe.workers.utils.TransactionItem;
 
 import java.util.HashMap;
@@ -10,83 +11,116 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public final class EventLogUtil {
+@UtilityClass
+public class EventLogUtil {
 
-    public static final String AMS_CONNECTOR = "ams_connector";
+    private static final String AMS_CONNECTOR = "ams_connector";
 
-    public static Event.Builder initFineractCall(String calledFrom,
-                                                 Integer disposalAccountId,
-                                                 Integer conversionAccountId,
-                                                 String internalCorrelationId,
-                                                 Event.Builder eventBuilder) {
-        eventBuilder.setSourceModule(AMS_CONNECTOR);
-        eventBuilder.setEventLogLevel(EventLogLevel.INFO);
-        eventBuilder.setEvent(calledFrom);
-        Map<String, String> correlationIds = new HashMap<>();
-        correlationIds.put("disposalAccountId", Integer.toString(disposalAccountId));
-        correlationIds.put("conversionAccountId", Integer.toString(conversionAccountId));
-        correlationIds.put("internalCorrelationId", internalCorrelationId);
-        eventBuilder.setCorrelationIds(correlationIds);
+    public Event.Builder initFineractCall(String event,
+                                          Integer disposalAccountId,
+                                          Integer conversionAccountId,
+                                          String internalCorrelationId,
+                                          Event.Builder eventBuilder) {
+        setHead(event, eventBuilder);
+        eventBuilder.setCorrelationIds(new CorrelationIdBuilder()
+                .disposalAccountId(disposalAccountId)
+                .conversionAccountId(conversionAccountId)
+                .internalCorrelationId(internalCorrelationId)
+                .build());
         return eventBuilder;
     }
 
-    public static Event.Builder initFineractBatchCall(String calledFrom,
-                                                      List<TransactionItem> batchItems,
-                                                      Integer disposalAccountId,
-                                                      Integer conversionAccountId,
-                                                      String internalCorrelationId,
-                                                      Event.Builder eventBuilder) {
-        eventBuilder.setSourceModule(AMS_CONNECTOR);
-        eventBuilder.setEventLogLevel(EventLogLevel.INFO);
-        eventBuilder.setEvent(calledFrom + ".AmsBatch");
-        Map<String, String> correlationIds = new HashMap<>();
-        correlationIds.put("disposalAccountId", Integer.toString(disposalAccountId));
-        correlationIds.put("conversionAccountId", Integer.toString(conversionAccountId));
-        // TODO should use operation text instead of the requestId
-        correlationIds.put("bacthItems", batchItems.stream()
-                .map(item -> item.requestId().toString())
-                .collect(Collectors.joining(",")));
-        correlationIds.put("internalCorrelationId", internalCorrelationId);
-        eventBuilder.setCorrelationIds(correlationIds);
+    public Event.Builder initFineractBatchCall(String event,
+                                               List<TransactionItem> batchItems,
+                                               Integer disposalAccountId,
+                                               Integer conversionAccountId,
+                                               String internalCorrelationId,
+                                               Event.Builder eventBuilder) {
+        setHead(event + ".AmsBatch", eventBuilder);
+        eventBuilder.setCorrelationIds(new CorrelationIdBuilder()
+                .disposalAccountId(disposalAccountId)
+                .conversionAccountId(conversionAccountId)
+                .internalCorrelationId(internalCorrelationId)
+                // TODO should use operation text instead of the requestId
+                .add("bacthItems", batchItems.stream()
+                        .map(item -> item.requestId().toString())
+                        .collect(Collectors.joining(",")))
+                .build());
         return eventBuilder;
     }
 
-    public static Event.Builder initFineractBatchCallOnUs(String calledFrom,
-                                                          List<TransactionItem> batchItems,
-                                                          Integer debtorDisposalAccountAmsId,
-                                                          Integer debtorConversionAccountAmsId,
-                                                          Integer creditorDisposalAccountAmsId,
-                                                          String internalCorrelationId,
-                                                          Event.Builder eventBuilder) {
-        eventBuilder.setSourceModule(AMS_CONNECTOR);
-        eventBuilder.setEventLogLevel(EventLogLevel.INFO);
-        eventBuilder.setEvent(calledFrom + ".AmsBatch");
-        Map<String, String> correlationIds = new HashMap<>();
-        correlationIds.put("debtorDisposalAccountAmsId", Integer.toString(debtorDisposalAccountAmsId));
-        correlationIds.put("debtorConversionAccountAmsId", Integer.toString(debtorConversionAccountAmsId));
-        correlationIds.put("creditorDisposalAccountAmsId", Integer.toString(creditorDisposalAccountAmsId));
-        // TODO should use operation text instead of the requestId
-        correlationIds.put("bacthItems", batchItems.stream()
-                .map(item -> item.requestId().toString())
-                .collect(Collectors.joining(",")));
-        correlationIds.put("internalCorrelationId", internalCorrelationId);
-        eventBuilder.setCorrelationIds(correlationIds);
+    public Event.Builder initFineractBatchCallOnUs(String event,
+                                                   List<TransactionItem> batchItems,
+                                                   Integer debtorDisposalAccountAmsId,
+                                                   Integer debtorConversionAccountAmsId,
+                                                   Integer creditorDisposalAccountAmsId,
+                                                   String internalCorrelationId,
+                                                   Event.Builder eventBuilder) {
+        setHead(event + ".AmsBatch", eventBuilder);
+        eventBuilder.setCorrelationIds(new CorrelationIdBuilder()
+                .add("debtorDisposalAccountAmsId", debtorDisposalAccountAmsId)
+                .add("debtorConversionAccountAmsId", debtorConversionAccountAmsId)
+                .add("creditorDisposalAccountAmsId", creditorDisposalAccountAmsId)
+                .internalCorrelationId(internalCorrelationId)
+                // TODO should use operation text instead of the requestId
+                .add("bacthItems", batchItems.stream()
+                        .map(item -> item.requestId().toString())
+                        .collect(Collectors.joining(",")))
+                .build());
         return eventBuilder;
     }
 
-    public static Event.Builder initZeebeJob(ActivatedJob activatedJob,
-                                             String event,
-                                             String internalCorrelationId,
-                                             String transactionGroupId,
-                                             Event.Builder eventBuilder) {
+    public Event.Builder initZeebeJob(ActivatedJob activatedJob,
+                                      String event,
+                                      String internalCorrelationId,
+                                      String transactionGroupId,
+                                      Event.Builder eventBuilder) {
+        setHead(event, eventBuilder);
+        eventBuilder.setCorrelationIds(new CorrelationIdBuilder()
+                .add("processInstanceKey", activatedJob.getProcessInstanceKey())
+                .add("transactionGroupId", transactionGroupId)
+                .internalCorrelationId(internalCorrelationId)
+                .build());
+        return eventBuilder;
+    }
+
+    private void setHead(String event, Event.Builder eventBuilder) {
         eventBuilder.setSourceModule(AMS_CONNECTOR);
         eventBuilder.setEventLogLevel(EventLogLevel.INFO);
         eventBuilder.setEvent(event);
-        Map<String, String> correlationIds = new HashMap<>();
-        correlationIds.put("processInstanceKey", Long.toString(activatedJob.getProcessInstanceKey()));
-        correlationIds.put("internalCorrelationId", internalCorrelationId);
-        correlationIds.put("transactionGroupId", transactionGroupId);
-        eventBuilder.setCorrelationIds(correlationIds);
-        return eventBuilder;
+    }
+
+    private static class CorrelationIdBuilder {
+
+        private static final String EMPTY = "N/A";
+        private final Map<String, String> correlationIds;
+
+        public CorrelationIdBuilder() {
+            correlationIds = new HashMap<>();
+        }
+
+        public CorrelationIdBuilder add(String id, Object value) {
+            correlationIds.put(id, value != null ? value.toString() : EMPTY);
+            return this;
+        }
+
+        public CorrelationIdBuilder internalCorrelationId(String internalCorrelationId) {
+            correlationIds.put("internalCorrelationId", internalCorrelationId != null ? internalCorrelationId : EMPTY);
+            return this;
+        }
+
+        public CorrelationIdBuilder disposalAccountId(Integer disposalAccountId) {
+            correlationIds.put("disposalAccountId", disposalAccountId != null ? Integer.toString(disposalAccountId) : EMPTY);
+            return this;
+        }
+
+        public CorrelationIdBuilder conversionAccountId(Integer conversionAccountId) {
+            correlationIds.put("conversionAccountId", conversionAccountId != null ? Integer.toString(conversionAccountId) : EMPTY);
+            return this;
+        }
+
+        public Map<String, String> build() {
+            return Map.copyOf(correlationIds);
+        }
     }
 }
