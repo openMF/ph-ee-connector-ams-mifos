6f4e4381a479c6eb85ad6f1c62b8343ade1c860d
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-09-29 13:18:54 +0200

    spring-aop based interceptor

---
+++ src/main/java/org/mifos/connector/ams/zeebe/ZeebeWorkerAspect.java
@@ -0,0 +1,27 @@
+package org.mifos.connector.ams.zeebe;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ZeebeWorkerAspect {
+    private static final Logger LOG = LoggerFactory.getLogger("zeebe-worker-aspect");
+
+    @Pointcut("execution(@io.camunda.zeebe.spring.client.annotation.ZeebeWorker * *(..))")
+    public void zeebeWorkerMethods() {
+    }
+
+    @Around("zeebeWorkerMethods()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        LOG.info("## worker entry: {}", joinPoint.getSignature().getName());
+        Object result = joinPoint.proceed();
+        LOG.info("## worker exit: {}", joinPoint.getSignature().getName());
+        return result;
+    }
+}
