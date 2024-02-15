4d09ac364426ff22657d3a167ab559006c5cf79c
Anover000 <140409886+Anover000@users.noreply.github.com>
2024-02-15 18:44:28 +0530

    zeebe dep 8.1.1->8.1.23, pollInt prop configurable, 1-> 10 (#61)

--- src/main/java/org/mifos/connector/ams/zeebe/ZeebeClientConfiguration.java
+++ src/main/java/org/mifos/connector/ams/zeebe/ZeebeClientConfiguration.java
@@ -17,10 +17,13 @@ public class ZeebeClientConfiguration {
     @Value("${zeebe.client.max-execution-threads}")
     private int zeebeClientMaxThreads;
 
+    @Value("${zeebe.client.poll-interval}")
+    private int zeebeClientPollInterval;
+
     @Bean
     public ZeebeClient setup() {
         return ZeebeClient.newClientBuilder().gatewayAddress(zeebeBrokerContactpoint).usePlaintext()
-                .defaultJobPollInterval(Duration.ofMillis(1)).defaultJobWorkerMaxJobsActive(2000)
+                .defaultJobPollInterval(Duration.ofMillis(zeebeClientPollInterval)).defaultJobWorkerMaxJobsActive(2000)
                 .numJobWorkerExecutionThreads(zeebeClientMaxThreads).build();
     }
 }
