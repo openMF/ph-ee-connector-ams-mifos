209246e79d07097ee9fe25731bf937c2519010e3
Kristof Jozsa <kristof.jozsa@gmail.com>
2024-02-26 10:19:57 +0100

    update fineract error message

--- src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/AbstractMoneyInOutWorker.java
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/AbstractMoneyInOutWorker.java
@@ -362,8 +362,8 @@ public abstract class AbstractMoneyInOutWorker {
                     }
                     case SC_FORBIDDEN -> {
                         String body = responseItem.getBody();
-                        if (body != null && (body.contains("error.msg.overdraft.not.allowed") || body.contains("error.msg.available.balance.violated"))) {
-                            log.error("Overdraft is not allowed for request [{}]", idempotencyKey);
+                        if (body != null && (body.contains("error.msg.current.insufficient.funds"))) {
+                            log.error("insufficient funds for request [{}]", idempotencyKey);
                             throw new ZeebeBpmnError("Error_InsufficientFunds", "Insufficient funds");
                         }
                         throw new ZeebeBpmnError("Error_CaughtException", "Forbidden");
