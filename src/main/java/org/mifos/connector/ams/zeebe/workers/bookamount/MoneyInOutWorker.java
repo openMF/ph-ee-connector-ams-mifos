69b4ffb214143dac86281c63a590c8a7c5f3a4cf
AszityM-dpc <martin.aszity@dpc.hu>
2024-04-26 15:07:05 +0200

    return a zeebe exception on database conflict

--- src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/MoneyInOutWorker.java
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/bookamount/MoneyInOutWorker.java
@@ -293,7 +293,7 @@ public class MoneyInOutWorker {
         switch (statusCode) {
             case SC_CONFLICT -> {
                 log.warn("Transaction request is already executing, has not completed yet");
-                return null;
+                throw new ZeebeBpmnError("Error_CaughtException", "Fineract returned a conflict error");
             }
             case SC_LOCKED -> {
                 log.info("Locking exception detected, retrying request");
