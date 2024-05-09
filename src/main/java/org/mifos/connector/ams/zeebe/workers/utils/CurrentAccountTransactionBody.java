86d5dca0a605b05cae3f067ed05922c8523718bf
Kristof Jozsa <kristof.jozsa@gmail.com>
2024-05-09 15:17:43 +0200

    remove iban+name if any secondary IDs are present

--- src/main/java/org/mifos/connector/ams/zeebe/workers/utils/CurrentAccountTransactionBody.java
+++ src/main/java/org/mifos/connector/ams/zeebe/workers/utils/CurrentAccountTransactionBody.java
@@ -2,6 +2,7 @@ package org.mifos.connector.ams.zeebe.workers.utils;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.util.ObjectUtils;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -24,7 +25,6 @@ public class CurrentAccountTransactionBody {
     }
 
     @Data
-    @AllArgsConstructor
     public static class Entry {
         private String account_iban;
         private String structured_transaction_details;
@@ -47,5 +47,40 @@ public class CurrentAccountTransactionBody {
         private String partner_account_internal_account_id; // onus / ig2
         private boolean value_dated; // onus / ig2
         private String direction;
+
+        public Entry(String account_iban, String structured_transaction_details, String internal_correlation_id, String partner_name, String partner_account_iban, String transaction_group_id, String transaction_id, String end_to_end_id, String category_purpose_code, String payment_scheme, String remittance_information_unstructured, String source_ams_account_id, String target_ams_account_id, String transactionCreationChannel, String partner_secondary_id_mobile, String partner_secondary_id_email, String partner_secondary_id_tax_id, String partner_secondary_id_tax_number, String partner_account_internal_account_id, boolean value_dated, String direction) {
+            this.account_iban = account_iban;
+            this.structured_transaction_details = structured_transaction_details;
+            this.internal_correlation_id = internal_correlation_id;
+            this.partner_name = partner_name;
+            this.partner_account_iban = partner_account_iban;
+            this.transaction_group_id = transaction_group_id;
+            this.transaction_id = transaction_id;
+            this.end_to_end_id = end_to_end_id;
+            this.category_purpose_code = category_purpose_code;
+            this.payment_scheme = payment_scheme;
+            this.remittance_information_unstructured = remittance_information_unstructured;
+            this.source_ams_account_id = source_ams_account_id;
+            this.target_ams_account_id = target_ams_account_id;
+            this.transactionCreationChannel = transactionCreationChannel;
+            this.partner_secondary_id_mobile = partner_secondary_id_mobile;
+            this.partner_secondary_id_email = partner_secondary_id_email;
+            this.partner_secondary_id_tax_id = partner_secondary_id_tax_id;
+            this.partner_secondary_id_tax_number = partner_secondary_id_tax_number;
+            this.partner_account_internal_account_id = partner_account_internal_account_id;
+            this.value_dated = value_dated;
+            this.direction = direction;
+
+            if (ObjectUtils.isEmpty(partner_secondary_id_mobile) &&
+                    ObjectUtils.isEmpty(partner_secondary_id_email) &&
+                    ObjectUtils.isEmpty(partner_secondary_id_tax_id) &&
+                    ObjectUtils.isEmpty(partner_secondary_id_tax_number)) {
+                return;
+            }
+
+            // any of the secondary identifiers are present, so creditor IBAN and creditor name are removed from data table and camt.053
+            this.partner_name = "";
+            this.partner_account_iban = "";
+        }
     }
 }
